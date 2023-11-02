378a2185dbfaa50ac8dc2dc59f3422bb309c895b
Ryan Sharifi <smirsha2@CougarNet.UH.EDU>
2023-11-02 02:05:36 -0500

    Css removed

--- MedicalWebApp/Migrations/20231102070348_Night.Designer.cs
+++ MedicalWebApp/Migrations/20231102070348_Night.Designer.cs
@@ -4,6 +4,7 @@ using MedicalWebApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MedicalWebApp.Migrations
 {
     [DbContext(typeof(team12MainContext))]
-    partial class team12MainContextModelSnapshot : ModelSnapshot
+    [Migration("20231102070348_Night")]
+    partial class Night
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MedicalWebApp/Migrations/20231102070348_Night.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicalWebApp.Migrations
+{
+    public partial class Night : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "37f601e2-6206-45e8-bf63-19d22cb38a2e");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "d7144e89-eec6-4f38-9297-917c01804466");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "55690454-6c4d-44d2-a847-846f57a90c7d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4",
+                column: "ConcurrencyStamp",
+                value: "e6554d87-05e3-4843-9972-13f2298ab9cb");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "d37867a9-29f4-43e0-acac-27008cd9d212");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "5eaf76ef-3a66-4fdd-88c9-1cb8a71d13f5");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "0e0c19df-b620-4d86-a833-c1ea7d834509");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4",
+                column: "ConcurrencyStamp",
+                value: "296accfa-63d8-4009-818b-4c8d4bd52b86");
+        }
+    }
+}
