c0e82168ac9c17ce30c67d4ca993292eb55eb714
Ryan Sharifi <smirsha2@CougarNet.UH.EDU>
2023-11-07 13:05:22 -0600

    css

--- MedicalWebApp/wwwroot/js/site.js
+++ MedicalWebApp/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var navToggle = document.querySelector('.nav-toggle');
+    var navigation = document.querySelector('.navigation');
+
+    navToggle.addEventListener('click', function () {
+        navigation.classList.toggle('active');
+        navToggle.classList.toggle('active');
+    });
+});
