745ff66a7c52fa22c30cb7412a01f58b03838350
Ryan Sharifi <123040383+ryan4sharifi@users.noreply.github.com>
2023-11-13 17:41:35 -0600

    Create AppointmentViewModel.cs

---
+++ MedicalWebApp/ViewModels/AppointmentViewModel.cs
@@ -0,0 +1,10 @@
+using MedicalWebApp.Models;
+
+namespace MedicalWebApp.ViewModels
+{
+    public class AppointmentViewModel
+    {
+        public Appointment Appointment { get; set; }
+        public Patient Patient { get; set; }
+    }
+}
